ba9aeaf1cb62704d7364cd9a2037f5b380721b17
Sila <rickyrichie001@gmail.com>
2024-05-16 04:15:19 +0300

    improve

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,19 @@
 const path = require('path');
 
+// Define the entry point for your application
+const entry = {
+  app: './js/app.js',
+};
+
+// Define the output configuration
+const output = {
+  path: path.resolve(__dirname, 'dist/js'),
+  clean: true,
+  filename: 'app.js',
+};
+
+// Export the configuration object
 module.exports = {
-  entry: {
-    app: './js/app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    clean: true,
-    filename: 'app.js',
-  },
+  entry,
+  output,
 };
