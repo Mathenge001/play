9fefb0b0604d97e3233759de4dd021f9084e6d32
Sila <rickyrichie001@gmail.com>
2024-05-16 04:13:20 +0300

    improve

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,9 +5,14 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    // Enables live reloading and hot module replacement
     liveReload: true,
     hot: true,
+    // Automatically opens the browser when dev server starts
     open: true,
-    static: ['./'],
+    // Specifies the directory where to serve the static files
+    static: { directory: './' },
+    // Additional devServer options can be added here
   },
 });
+
